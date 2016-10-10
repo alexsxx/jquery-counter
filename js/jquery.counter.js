433a738f4cb8b7cfa5d5743be7700e5c98487fed
alexsxx <alessandro.nati.94@gmail.com>
2016-10-10 14:48:13 +0200

    Add files via upload

---
+++ js/jquery.counter.js
@@ -0,0 +1,77 @@
+(function ($) {
+
+    $.fn.counter = function (m, opts) {
+
+        var options = $.extend({
+            start: 0
+        }, opts);
+
+        var methods = {
+            init: function () {
+                this.each(function () {
+                    $(this).addClass('countdownHolder');
+
+                    $('<span class="count' + this + '">').html(
+                        '<span class="position" data-digit="' +
+                        options.start +
+                        '">\
+					<span class="digit static">' +
+                        options.start +
+                        '</span>\
+				</span>'
+                    ).appendTo($(this));
+                });
+            },
+            increment: function (number, callback) {
+
+                var position = this.find('.position');
+                var digit = position.find('.digit')
+
+                // We are already showing this number
+                if (position.data('digit') == number)
+                    return false;
+
+                position.data('digit', number);
+
+                var replacement = $('<span>', {
+                    'class': 'digit',
+                    css: {
+                        top: '-2.1em',
+                        opacity: 0
+                    },
+                    html: number
+                });
+
+                digit.animate({
+                    top: '1.2em',
+                    opacity: 0
+                }, 100)
+                    .delay(300);
+
+                digit
+                    .before(replacement)
+                    .removeClass('static')
+                    .animate({ top: '2.5em', opacity: 0 }, 'fast', function () {
+                        digit.remove();
+                    })
+
+                replacement
+                    .delay(100)
+                    .animate({ top: 0, opacity: 1 }, 'fast', function () {
+                        replacement.addClass('static');
+                    });
+
+                if (callback != undefined)
+                    callback();
+            }
+        }
+
+        if (methods[m])
+            return methods[m].apply(this, Array.prototype.slice.call(arguments, 1));
+        else if (typeof m === "object" || !m)
+            return methods.init.apply(this, arguments);
+
+        return this;
+    };
+
+})(jQuery);
